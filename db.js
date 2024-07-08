f9d8466a3cd0d5afb338c27b5a384a4496b28630
zee-ham-su <hsufiian@yahoo.com>
2024-07-08 17:55:06 +0000

    Update database configuration for deployment

--- db.js
+++ db.js
@@ -2,13 +2,30 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = process.env;
+let sequelize;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: 'postgres',
-  port: DB_PORT,
-});
+if (process.env.DATABASE_URL) {
+  // Connect to the database using the DATABASE_URL provided by Heroku
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // <<<<<<< YOU MAY NEED THIS
+      }
+    }
+  });
+} else {
+  // Fallback to your local configuration if DATABASE_URL is not available
+  const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = process.env;
+
+  sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: 'postgres',
+    port: DB_PORT,
+  });
+}
 
 // Test the database connection
 sequelize
@@ -18,7 +35,7 @@ sequelize
   })
   .catch((error) => {
     console.error('Unable to connect to the database:', error);
-
   });
+
 // Export the Sequelize instance
 module.exports = sequelize;
